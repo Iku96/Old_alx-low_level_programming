7a7aa2e7955ee61511a3ae1e682ffe7e87e5fc1c
Iku96 <ikumwana@gmail.com>
2022-12-08 17:26:20 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 /**
  * main - Short description, single line
- * @parameterx: Description of parameter x
 (* a blank line
  * Description: Longer description of the function)?
 (* section header: Section description)*
  * Return: Returns zero
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	getchar();
