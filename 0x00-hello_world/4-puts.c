b93249bd8b639b1f3773fabaa750ee54a95571dd
Iku96 <ikumwana@gmail.com>
2022-12-08 16:09:41 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
-int main(){
-  
-  puts("\"Programming is like building a multilingual puzzle"); }
+int main() {
+   puts("\"Programming is like building a multilingual puzzle.");
+   getchar();
+   return 0;
+}
