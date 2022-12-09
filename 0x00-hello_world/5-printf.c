1e484f3abb2cd5e000a743c05607c0d682519aef
Iku96 <ikumwana@gmail.com>
2022-12-08 17:18:25 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * function_name - Main void funtion
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	getchar();
+	return (0);
+}
