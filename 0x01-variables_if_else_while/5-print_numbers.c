9eedb7e1eff23259a7616017b905928b0a7b2e6a
Iku96 <ikumwana@gmail.com>
2022-12-09 19:10:44 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdio.h>
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Returns zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num=0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
