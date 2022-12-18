8614fa17e89c06fe3eacd5bb8e8d8e34a9415ca2
Iku96 <ikumwana@gmail.com>
2022-12-17 22:09:07 -0800

    corrections qn 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * more_numbers
- * @i and @j to represent rows and columns 
+ * more_numbers - nmbr fct
+ * Bwave ICT file
  * Return: no return
  */
 
 void more_numbers(void)
 {
-	int i, j;
-	
+
+int i, j;
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
