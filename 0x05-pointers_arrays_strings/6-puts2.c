a690b6ed3929a91ba85bd08f0d2ab4e5bfed7d06
Iku96 <ikumwana@gmail.com>
2022-12-20 18:57:54 -0800

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
  #include "main.h"
 
 /**
- * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ * puts2 - Write a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
  *
  * @s: This pointer point to a char or string
  *
