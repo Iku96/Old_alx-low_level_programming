bbafe898a4963637f5dce239b26fb1e691df54b6
Iku96 <ikumwana@gmail.com>
2023-01-04 18:49:41 -0800

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,28 +5,44 @@
  *
  * Return: square root of a number.
  */
-int _sqrt_recursion(int n)
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+int find_sqrt(int num, int root)
 {
-	return _sqrt_recursion_helper(n, 1, n);
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
 }
 
-int _sqrt_recursion_helper(int n, int min, int max)
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
+ */
+int _sqrt_recursion(int n)
 {
-	if (min > max)
-	{
+	int root = 0;
+
+	if (n < 0)
 		return (-1);
-	}
-	int mid = (min + max) / 2;
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return _sqrt_recursion_helper(n, mid + 1, max);
-	}
-	else
-	{
-		return _sqrt_recursion_helper(n, min, mid - 1);
-	}
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
