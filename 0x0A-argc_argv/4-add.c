ef48b65b63003c7e3587bfc7ab30cbad0266908c
Iku96 <ikumwana@gmail.com>
2023-01-11 07:33:18 -0800

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry Point
+ * @argc: arguments count
+ * @argv: array pointing to arguments
+ * Return: 0 if correct input, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 0, len;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		len = strlen(argv[i]);
+		for (int j = 0; j < len; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
