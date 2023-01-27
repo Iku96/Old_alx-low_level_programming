2b20f7da289cbd6b77b8a779e4918facd482f402
Iku96 <ikumwana@gmail.com>
2023-01-26 18:52:02 -0800

    Singly linked list project first major commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_node - Function that adds a new node at the beginning of a list_t list
+ * @head: input
+ * @str: string
+ * Return: 0
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i, count = 0;
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
